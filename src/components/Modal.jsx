5f5af197d945a1bf2c9f0fc7f2eb0888c4ab0955
Shivyani Rathod <shivyani@Shivyanis-MacBook-Air.local>
2023-11-08 01:18:09 +0530

    added modal as components

---
+++ src/components/Modal.jsx
@@ -0,0 +1,17 @@
+import "./modal.css";
+
+const Modal = ({ handleClose, show, children }) => {
+  const showHideClassName = show
+    ? "modal display-block modal-backdrop"
+    : "modal display-none";
+
+  return (
+    <div className={showHideClassName} onClick={handleClose}>
+      <section className="modal-main" onClick={(e) => e.stopPropagation()}>
+        {children}
+      </section>
+    </div>
+  );
+};
+
+export default Modal;
