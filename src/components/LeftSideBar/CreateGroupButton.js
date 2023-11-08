524194f2b2016fb845e758b4a3b8633ab9e571ea
Shivyani Rathod <shivyani@Shivyanis-MacBook-Air.local>
2023-11-09 00:45:16 +0530

    dialog and button fixes

--- src/components/LeftSideBar/CreateGroupButton.js
+++ src/components/LeftSideBar/CreateGroupButton.js
@@ -3,6 +3,7 @@ import React from "react";
 function CreateGroupButton({ handleCreateGroup }) {
   return (
     <button className="create-group-button" onClick={handleCreateGroup}>
+      <img src="/assets/+.svg" alt="Create Group" />
       Create Notes Group
     </button>
   );
