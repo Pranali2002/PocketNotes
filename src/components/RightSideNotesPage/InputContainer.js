91874c1a7a3981c70b6da1b6b07c5e6a10fed8c4
Shivyani Rathod <shivyani@Shivyanis-MacBook-Air.local>
2023-11-09 15:10:34 +0530

    scrolling updated

--- src/components/RightSideNotesPage/InputContainer.js
+++ src/components/RightSideNotesPage/InputContainer.js
@@ -2,25 +2,25 @@ import React from "react";
 
 function InputContainer({ noteText, handleNoteChange, handleNoteSubmit }) {
   return (
-    <form className="input-container" onSubmit={handleNoteSubmit}>
-      {/* <input
-        type="text"
-        placeholder="Enter your text here.........."
-        value={noteText}
-        className="input-note"
-        onChange={handleNoteChange}
-      /> */}
-      <textarea
-        className="input-note"
-        onChange={handleNoteChange}
-        value={noteText}
-        placeholder="Enter your text here..........."
-        rows={5}
-      ></textarea>
-      <button className="enter-button">
-        <img src="/assets/enter.svg" alt="Enter" />
-      </button>
-    </form>
+      <form className="input-container" onSubmit={handleNoteSubmit}>
+        {/* <input
+          type="text"
+          placeholder="Enter your text here.........."
+          value={noteText}
+          className="input-note"
+          onChange={handleNoteChange}
+  /> */}
+        <textarea
+          className="input-note"
+          onChange={handleNoteChange}
+          value={noteText}
+          placeholder="Enter your text here..........."
+          rows={5}
+        ></textarea>
+        <button className="enter-button">
+          <img src="/assets/enter.svg" alt="Enter" />
+        </button>
+      </form> 
   );
 }
 
